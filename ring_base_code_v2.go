5729fc6a43f285e55a19bf3d83f99f05e511dbf7
Jhanos <joao.sbardelotto@edu.pucrs.br>
2025-05-07 20:49:51 -0300

    FUNCIONANDO

--- ring_base_code_v2.go
+++ ring_base_code_v2.go
@@ -31,12 +31,23 @@ func ElectionControler(in chan int) {
 
 	// comandos para o anel iciam aqui
 
-	// mudar o processo 0 - canal de entrada 3 - para falho (defini mensagem tipo 2 pra isto)
+	temp.tipo = 5
+	chans[0] <- temp
+	fmt.Printf("Controle: confirmação %d\n", <-in)
+	chans[1] <- temp
+	fmt.Printf("Controle: Canal 0 e 1 falho\n")
+	fmt.Printf("Controle: confirmação %d\n", <-in)
 
-	temp.tipo = 4
+	temp.tipo = 1
 	chans[2] <- temp
 	fmt.Printf("Controle: Iniciando eleição\n")
 	fmt.Printf("Controle: confirmação %d\n", <-in)
+
+	temp.tipo = 6
+	chans[0] <- temp
+	chans[1] <- temp
+	chans[2] <- temp
+	chans[3] <- temp
 
 	fmt.Println("\n   Processo controlador concluído\n")
 }
@@ -47,69 +58,82 @@ func ElectionStage(TaskId int, in chan mensagem, out chan mensagem, leader int) 
 	var actualLeader int
 	var bFailed bool = false
 	var isElection bool = false
+	var running bool = true
 
 	actualLeader = leader
 
-	for {
+	for running {
 		temp := <-in
 
 		fmt.Printf("%2d: recebi mensagem %d, [ %d, %d, %d ]\n", TaskId, temp.tipo, temp.corpo[0], temp.corpo[1], temp.corpo[2])
 
 		switch temp.tipo {
-		// case 1:
-		// 	{
-		// 		if temp.corpo[0] == TaskId {
-		// 			fmt.Printf("%2d: eleição concluída\n", TaskId)
-		// 			controle <- -5
-		// 			return
-		// 		}
-		// 		isElection = true
-		// 		fmt.Printf("%2d: eleição iniciada: %v\n", TaskId, isElection)
-		// 		out <- temp
-		// 		controle <- -5
-		// 	}
+		/*
+			1- Inicia eleição
+			2- Votação
+			3- Confirmação de líder
+			4- Revive
+			5- Morre
+			6- Termina
+		*/
 		case 1:
 			{
 				isElection = true
-				if temp.corpo[0] > TaskId {
-					temp.corpo[0] = TaskId
-				}
-				fmt.Printf("%2d: eleição iniciada: %v\n", TaskId, isElection)
+				temp.corpo[0] = TaskId
+				fmt.Printf("%2d: eleição iniciada: %v \n", TaskId, isElection)
 				fmt.Printf("%2d: votei em %d\n", TaskId, temp.corpo[0])
+				temp.tipo = 2
 				out <- temp
+				temp = <-in
+				fmt.Printf("%2d: ESTOU VIVO!!!\n", TaskId)
+				temp.tipo = 3
+				fmt.Printf("%2d: Confirmando líder\n", TaskId)
+				out <- temp
+				temp = <-in
+				fmt.Printf("%2d: Eleição concluída\n", TaskId)
 				controle <- -5
 			}
 		case 2:
 			{
+				if !bFailed {
+					isElection = true
+					if temp.corpo[0] > TaskId {
+						fmt.Printf("%2d: mudei meu voto para %d\n", TaskId, temp.corpo[0])
+						temp.corpo[0] = TaskId
+					}
+					fmt.Printf("%2d: eleição iniciada: %v\n", TaskId, isElection)
+					fmt.Printf("%2d: votei em %d\n", TaskId, temp.corpo[0])
+				}
+				out <- temp
+				// controle <- -5
+			}
+		case 3:
+			{
+				fmt.Printf("%2d: ESTOU VIVO!!!\n", TaskId)
 				isElection = false
 				actualLeader = temp.corpo[0]
 				fmt.Printf("%2d: lider atual %d\n", TaskId, actualLeader)
-				fmt.Printf("%2d: eleição concluída\n", TaskId)
 				out <- temp
-				controle <- -5
+				// controle <- -5
 			}
-		case 3:
+		case 4:
 			{
 				bFailed = false
 				fmt.Printf("%2d: falho %v \n", TaskId, bFailed)
 				fmt.Printf("%2d: lider atual %d\n", TaskId, actualLeader)
 				controle <- -5
 			}
-		case 4:
+		case 5:
 			{
-				isElection = true
-				temp.corpo[0] = TaskId
-				fmt.Printf("%2d: eleição iniciada: %v \n", TaskId, isElection)
-				fmt.Printf("%2d: votei em %d\n", TaskId, temp.corpo[0])
-				temp.tipo = 1
-				out <- temp
-				temp = <-in
-				fmt.Printf("%2d: ESTOU VIVO!!!\n", TaskId)
-				fmt.Printf("Tipo da mensagem: %d\n", temp.tipo)
-				temp.tipo = 2
-				fmt.Printf("%2d: Confirmando líder\n", TaskId)
-				out <- temp
+				bFailed = true
+				fmt.Printf("%2d: falho %v \n", TaskId, bFailed)
+				fmt.Printf("%2d: lider atual %d\n", TaskId, actualLeader)
 				controle <- -5
+			}
+		case 6:
+			{
+				running = false
+				fmt.Printf("%2d: TERMINEI!!!!!!!!!\n", TaskId)
 			}
 		default:
 			{
@@ -126,10 +150,10 @@ func main() {
 
 	// criar os processo do anel de eleicao
 
-	go ElectionStage(0, chans[3], chans[0], 0) // este é o lider
-	go ElectionStage(1, chans[0], chans[1], 0) // não é lider, é o processo 0
-	go ElectionStage(2, chans[1], chans[2], 0) // não é lider, é o processo 0
-	go ElectionStage(3, chans[2], chans[3], 0) // não é lider, é o processo 0
+	go ElectionStage(3, chans[3], chans[0], 0) // este é o lider
+	go ElectionStage(0, chans[0], chans[1], 0) // não é lider, é o processo 0
+	go ElectionStage(1, chans[1], chans[2], 0) // não é lider, é o processo 0
+	go ElectionStage(2, chans[2], chans[3], 0) // não é lider, é o processo 0
 
 	fmt.Println("\n   Anel de processos criado")
 
